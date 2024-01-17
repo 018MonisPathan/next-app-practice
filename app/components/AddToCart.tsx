46b72e33bb33af5ef94311e633d8d8d1460fbde5
Monis <19bmiit018@gmail.com>
2024-01-17 16:57:07 +0530

    Project

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { log } from 'console'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={()=> console.log('Click')}></button>
+    </div>
+  )
+}
+
+export default AddToCart
